2eb31e07e6e3a837eab2a4a3de7fa4f05f1434cd
Amy Holt <amy.elizabeth.holt@gmail.com>
2022-01-26 14:56:16 -0700

    Initial commit

---
+++ oop.rb
@@ -0,0 +1,30 @@
+# Write a class called Unicorn
+# it should have a dynamic name attribute
+#  it should have a color attribute, that is silver by default
+# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+
+
+
+#  Write a class called Vampire
+#  it should have a dynamic name attribute
+#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
+#  it should have a thirsty attribute, that is true by default
+#  it should have a drink method. When called, the thirsty attribute changes to false
+
+
+#  Write a Dragon class
+#  it should have a dynamic name attribute (string)
+#  it should have a dynamic rider attribute (string)
+#  it should have a dynamic color attribute (string)
+#  it should have a is_hungry attribute that is true by default
+#  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+
+
+#  Write a Hobbit class
+#  it should have a dynamic name attribute (string)
+#  it should have a dynamic disposition attribute (string)
+#  it should have an age attribute that defaults to 0
+#  it should have a celebrate_birthday method. When called, the age increases by 1
+#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
+#  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
+#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
---
+++ self-eval.md
@@ -0,0 +1,5 @@
+# Self Evaluation
+
+- This final CFU encompassed everything you've learned in Mod 0, and maybe even required some learning outside of what you've explicitly been taught. How did it feel as you faced these challenges?
+- How is your workflow going, in terms of taking breaks, staying focused, etc.?
+- What do you need to do to be ready for intense technical lessons and challenges like this, for 6 hours/day next week? What support do you need?
